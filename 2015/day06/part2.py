1d5b929e254484e07bf9afdb58c8d2d6209997ca
Ivan Peťko <ivanko.petko@gmail.com>
2022-07-06 15:27:25 +0200

    solve day 6

---
+++ 2015/day06/part2.py
@@ -0,0 +1,25 @@
+import re
+
+readLinePattern = "([\w\s]+)\s(\d+),(\d+)\s\w+\s(\d+),(\d+)"
+
+lightArray = [[0] * 1000 for _ in range(1000)]
+
+
+def changeLights(instructions) -> None:
+    for i in range(int(instructions.group(2)), int(instructions.group(4)) + 1):
+        for j in range(int(instructions.group(3)), int(instructions.group(5)) + 1):
+            if "on" in instructions.group(1):
+                lightArray[i][j] += 1
+            elif "off" in instructions.group(1):
+                if lightArray[i][j] != 0:
+                    lightArray[i][j] -= 1
+            else:
+                lightArray[i][j] += 2
+    return
+
+
+with open("2015\day06\input", "r") as file:
+    for line in file:
+        match = re.match(readLinePattern, line.strip())
+        changeLights(match)
+    print(sum(sum(lightArray, [])))
