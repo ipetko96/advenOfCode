1468d041df2e7f5a5ef442423f41d81af5a32496
Ivan Peťko <ivanko.petko@gmail.com>
2022-09-06 22:03:06 +0200

    wip

---
+++ 2015/day11/part1.py
@@ -0,0 +1,16 @@
+with open("2015//day11//input") as file:
+    oldPass = file.read().strip()
+
+
+def banned(input: str) -> bool:
+    if input in "iol":
+        return True
+    return False
+
+
+def increasing(input: str) -> bool:
+    return
+
+
+def pair(input: str) -> bool:
+    return
