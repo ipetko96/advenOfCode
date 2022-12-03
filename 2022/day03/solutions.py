dce83e9570e8de1e40e0258ee9acedcd9072824e
Ivan Peťko <ivanko.petko@gmail.com>
2022-12-03 08:41:07 +0100

    solve day03

---
+++ 2022/day03/solutions.py
@@ -0,0 +1,39 @@
+from difflib import SequenceMatcher
+
+# part 1
+count = 0
+with open("2022\\day02\\input", "r") as file:
+    for line in file:
+        line = line.strip()
+        a, b = line[:len(line) // 2], line[len(line) // 2:]
+        match = SequenceMatcher(None, a, b).find_longest_match()
+        answer = a[match.a:match.a + match.size]
+        if answer.isupper():
+            count += ord(answer[0]) - 38
+        else:
+            count += ord(answer[0]) - 96
+
+print(count)
+
+# part 2
+rucksacks = []
+count = 0
+with open("2022\\day02\\input", "r") as file:
+    for line in file:
+        rucksacks.append(line.strip())
+
+for i in range(0, len(rucksacks), 3):
+    sublist = []
+    sublist.append(rucksacks[i])
+    sublist.append(rucksacks[i + 1])
+    sublist.append(rucksacks[i + 2])
+    for j in min(sublist):
+        if j in rucksacks[i] and j in rucksacks[i + 1] and j in rucksacks[i + 2]:
+            if j.isupper():
+                count += ord(j) - 38
+                break
+            else:
+                count += ord(j) - 96
+                break
+
+print(count)
