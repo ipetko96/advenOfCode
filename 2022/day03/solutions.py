aea046fe0e241d59dcefc716c5e2dbd276a00f19
Ivan Peťko <ivanko.petko@gmail.com>
2023-01-21 15:27:15 +0100

    optimize solutions

--- 2022/day03/solutions.py
+++ 2022/day03/solutions.py
@@ -4,8 +4,7 @@ from difflib import SequenceMatcher
 count = 0
 with open("2022\\day03\\input", "r") as file:
     for line in file:
-        line = line.strip()
-        a, b = line[:len(line) // 2], line[len(line) // 2:]
+        a, b = line.strip()[:len(line) // 2], line[len(line) // 2:]
         match = SequenceMatcher(None, a, b).find_longest_match()
         answer = a[match.a:match.a + match.size]
         if answer.isupper():
@@ -23,10 +22,7 @@ with open("2022\\day03\\input", "r") as file:
         rucksacks.append(line.strip())
 
 for i in range(0, len(rucksacks), 3):
-    sublist = []
-    sublist.append(rucksacks[i])
-    sublist.append(rucksacks[i + 1])
-    sublist.append(rucksacks[i + 2])
+    sublist = rucksacks[i:i + 3]
     for j in min(sublist):
         if j in rucksacks[i] and j in rucksacks[i + 1] and j in rucksacks[i + 2]:
             if j.isupper():
