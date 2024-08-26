f7918c3a399e1905c2e87c2251c6ad0ce746236e
Ivan Peťko <ivanko.petko@gmail.com>
2024-08-26 21:54:23 +0200

    WIP maze

--- 2021/day15/part1.py
+++ 2021/day15/part1.py
@@ -1,19 +1,25 @@
 board = []
-rules = []
+table = []
 unvisited = []
 bigNumber = 999
 
-with open('2021\\day15\\tinput', 'r') as file:
+with open("2021\\day15\\tinput", "r") as file:
     for line in file:
         board.append(list(line.strip()))
 
 for i in range(len(board)):
     for j, value in enumerate(board[i]):
         if i == 0 and j == 0:
-            rules.append({"vertex": [i, j], "shortest": 0, "previous": ""})
+            table.append({"vertex": [i, j], "shortest": 0, "previous": ""})
         else:
-            rules.append(
-                {"vertex": [i, j],"value": board[i][j], "shortest": bigNumber, "previous": []})
+            table.append(
+                {
+                    "vertex": [i, j],
+                    "value": board[i][j],
+                    "shortest": bigNumber,
+                    "previous": [],
+                }
+            )
         unvisited.append([i, j])
 print()
 
@@ -27,40 +33,55 @@ def get_horizontal_and_vertical_neighbours(arr, x, y):
     """
     neighbors = []
 
-    if x == 0 or x == len(arr) - 1 or y == 0 or y == len(arr[x]) - 1:
+    if y == 0 or y == len(arr) - 1 or x == 0 or x == len(arr[y]) - 1:
         # corners
-        new_neighbors = []
-        if x != 0:
-            new_neighbors.append({"vertex": [x - 1][y], "value": arr[x - 1][y]})  # top neighbor
-        if y != len(arr[x]) - 1:
-            new_neighbors.append({"vertex": [x][y + 1], "value": arr[x][y + 1]})  # right neighbor
-        if x != len(arr) - 1:
-            new_neighbors.append({"vertex": [x + 1][y], "value": arr[x + 1][y]})  # bottom neighbor
         if y != 0:
-            new_neighbors.append({"vertex": [x][y - 1], "value": arr[x][y - 1]})  # left neighbor
+            neighbors.append(
+                {"vertex": [y - 1, x], "value": arr[y - 1][x]}
+            )  # top neighbor
+        if x != len(arr[y]) - 1:
+            neighbors.append(
+                {"vertex": [y, x + 1], "value": arr[y][x + 1]}
+            )  # right neighbor
+        if y != len(arr) - 1:
+            neighbors.append(
+                {"vertex": [y + 1, x], "value": arr[y + 1][x]}
+            )  # bottom neighbor
+        if x != 0:
+            neighbors.append(
+                {"vertex": [y, x - 1], "value": arr[y][x - 1]}
+            )  # left neighbor
 
     else:
         # add neighbors
-        new_neighbors = [{
-            arr[x - 1][y],  # top neighbor
-            arr[x][y + 1],  # right neighbor
-            arr[x + 1][y],  # bottom neighbor
-            arr[x][y - 1]   # left neighbor
-        }]
+        neighbors = [
+            {
+                arr[y - 1][x],  # top neighbor
+                arr[y][x + 1],  # right neighbor
+                arr[y + 1][x],  # bottom neighbor
+                arr[y][x - 1],  # left neighbor
+            }
+        ]
 
-        new_neighbors.append({
+    neighbors.append(
+        {
             "index": x * len(arr[x]) + j,
             "value": value,
-            "neighbors": new_neighbors,
-            "shortestPath": 999})
+            "neighbors": neighbors,
+            "shortestPath": 999,
+        }
+    )
 
     return neighbors
 
 
 while len(unvisited) != 0:
     currenVertex = unvisited[0]
-    for neighbor in get_horizontal_and_vertical_neighbours(board, currenVertex[0], currenVertex[1]):
+    for neighbor in get_horizontal_and_vertical_neighbours(
+        board, currenVertex[1], currenVertex[0]
+    ):
         print(neighbor)
+    unvisited.remove(currenVertex)
 
     # for i in range(len(board)):
     #     for j, value in enumerate(board[i]):
